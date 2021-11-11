a9fb1a568b2580ee342f159d9e10499cd64040ef
Miche Machine <elmiche@latinooutdoors.org>
2021-11-11 16:48:19 -0700

    add plugins and local images

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,15 @@ const HomePage = () => {
   return (
     <Layout pageHeading='Home' pageTitle='Home Page'> 
     <p>Welcome to my virtual space!</p>
-    <StaticImage
+    {/* <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
+      /> */}
+      
+    <StaticImage 
+    src="../images/pajaritoguava.jpg" 
+    alt="below angle of a bird perched on a branch, eating a guava fruit"
+    />
     </Layout>
       )
 }
