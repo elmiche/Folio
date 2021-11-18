d93ceade094999409b533c037cdcc5d4945de859
Miche Machine <elmiche@latinooutdoors.org>
2021-11-18 14:57:05 -0700

    change background

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,17 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-
+import { StaticImage } from 'gatsby-plugin-image'
 import { Container } from 'postcss'
 
 const Hero = ({ pageTitle, pageHeading, children }) => {
   return (
     <div className="bg-light-blue-300 ...">
-      
-    
       <h1 className="text-4xl p-4 ">Interweaving the natural world with the world wide web</h1>
-    
-
-
+      <StaticImage 
+          className="object-none h-full md:w-auto "
+          src="../images/deerhead-peote-ceremony.jpg" 
+          alt="a deerhead mounted on a staff with intricate web design in antlers, decorated with blue bird feathers and peote cactus drawings"
+          />
     </div>
 
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,15 @@ const HomePage = () => {
   return (
     <Layout pageHeading='The Cyborg Naturalist' pageTitle='Home Page'> 
     
+    <div>
+    <Hero></Hero>
     
-    <div class="conatiner bg-origin-border rounded-lg p-4 bg-white bg-opacity-95 flex flex-row space-y-5"> 
+      <p className="text-center"> Welcome to my virtual space</p>
+      <p className="text-center"> This site contains some information about <br></br> my work and my ambitions</p>
+    </div>
+
+{/*     
+    <div class="conatiner bg-cover bg-origin-border rounded-lg p-4 bg-white bg-opacity-95 flex flex-row space-y-5"> 
       <div className="object-cover object-position:left h-60 w-full p-4">
         <div className="object-none">
         <Hero></Hero>
@@ -25,11 +32,11 @@ const HomePage = () => {
       </div>
         
     </div>
-          <div className=" text-lg rounded-lg container bg-origin-border p-4 bg-white bg-opacity-95 object-top flex flex-col">
+          <div className=" text-lg rounded-lg bg-origin-border p-4 bg-white bg-opacity-95 object-top flex flex-col">
             
             <p className="text-center"> Welcome to my virtual space</p>
               <p className="text-center"> This site contains some information about <br></br> my work and my ambitions</p>
-          </div>  
+          </div>   */}
 
     {/* <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
